84521dab76a24589b6149399994396599870a317
Aman Mishra <amanmishra1972007@gmail.com>
2025-09-04 20:53:14 +0530

    Add files via upload

---
+++ day15Q29.c
@@ -0,0 +1,26 @@
+// Q29: Write a program to calculate the factorial of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/ 
+# include <stdio.h>
+int main() {
+    int n, fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    printf("Factorial of %d is %d\n", n, fact);
+    return 0;
+}   
